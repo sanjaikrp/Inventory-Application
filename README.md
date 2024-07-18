83c8cc8cfa34b26a180b4037517101ea197bbe33
Sanjai-949 <jaisan262@gmail.com>
2024-07-18 22:36:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# InventoryApplication
+
+![GIF](https://github.com/nate51315/InventoryApplication/blob/master/InventoryApp.gif)
+
+### Inventory Application
+This is a Windows Forms application written in C# with a MySQL back-end to manage inventory, orders, and sales records.
+
+### Requirements
+* Visual Studio IDE
+* SQL Server
+* SQL Server Management Studio
+
+## Installation
+This section will help get the requirements for the app.
+
+### Visual Studios IDE Installation
+1. View Microsoft's official Visual Studios [download page](https://visualstudio.microsoft.com/downloads/)
+2. Select Visual Studios 2019 Community and install
+3. After the installation completes open Visual Studio Installer
+4. Select and install 
+    * .NET Desktop Development
+    * Data Storage and Processing
+    
+### SQL Server Installation
+1. View Microsoft's official SQL Server [download page](https://www.microsoft.com/en-us/sql-server/sql-server-downloads)
+2. Select Developer or Express then download
+3. Choose the basic installation type
+4. Install at default directory location
+
+### SQL Server Management Studio Installation
+1. View Microsoft's official (SSMS) [download page](https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver15)
+2. Click the link to download SSMS
+3. Follow the steps and click install at the end
+4. After the setup is complete launch SSMS
+5. Create a server or log into one
+
+ ## Configuration
+ 1. Create an empty database using MySQL Server.
+ 2. Locate the file called scripts within this repository.
+ 3. Import the file to populate the database and import the stored procedures.
+ 
+ 
+## License
+This project is licensed under MIT. Please see [LICENSE file](https://github.com/nate51315/InventoryApplication/blob/master/LICENSE)
---
+++ scripts/scripts.sql
@@ -0,0 +1,96 @@
+﻿USE [InventoryDatabase]
+GO
+/****** Object:  Table [dbo].[Categories]    Script Date: 2/15/2020 10:09:15 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categories](
+	[CategoryID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [nvarchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[CategoryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Customers]    Script Date: 2/15/2020 10:09:15 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customers](
+	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[Phone] [nvarchar](20) NULL,
+	[Email] [nvarchar](90) NOT NULL,
+	[City] [nvarchar](50) NOT NULL,
+	[Address] [nvarchar](50) NOT NULL,
+	[Balance] [decimal](15, 2) NOT NULL,
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Orders]    Script Date: 2/15/2020 10:09:15 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Orders](
+	[OrderID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerID] [int] NOT NULL,
+	[OrderDate] [date] NOT NULL,
+	[TotalAmount] [decimal](15, 2) NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Products]    Script Date: 2/15/2020 10:09:15 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[ProductID] [int] IDENTITY(1,1) NOT NULL,
+	[ProductName] [nvarchar](50) NOT NULL,
+	[ProductPrice] [decimal](15, 2) NOT NULL,
+	[ProductQuantity] [int] NOT NULL,
+	[CategoryName] [nvarchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Sales]    Script Date: 2/15/2020 10:09:15 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Sales](
+	[SaleID] [int] IDENTITY(1,1) NOT NULL,
+	[OrderID] [int] NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[ProductName] [nvarchar](50) NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[TotalPrice] [decimal](15, 2) NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 2/15/2020 10:09:15 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[LoginName] [nvarchar](40) NOT NULL,
+	[PasswordHash] [binary](64) NOT NULL,
+	[FirstName] [nvarchar](40) NULL,
+	[LastName] [nvarchar](40) NULL,
+	[Salt] [uniqueidentifier] NULL,
+ CONSTRAINT [PK_User_UserID] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
